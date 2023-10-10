30ea67a776e1a33b9bf4221adf349df80a9de833
iamkathan <shethkathan4@gmail.com>
2023-10-10 23:05:08 +0100

    message component has been completed

--- Home/views.py
+++ Home/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render, HttpResponse
+from datetime import datetime
+from Home.models import Index
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
+    if request.method == "POST":
+        name = request.POST.get("name")
+        email = request.POST.get("email")
+        subject = request.POST.get("subject")
+        message = request.POST.get("message")
+        index = Index(name=name, email=email, subject=subject, message=message, date=datetime.today())
+        index.save()
+        messages.success(request, "Your message has been sent")
     return render(request, 'index.html')
